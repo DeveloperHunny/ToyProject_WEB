3c255113efc75f51227828aabc67cbb21e72ccb1
eogns0321 <eogns0321@gmail.com>
2021-06-21 10:34:34 -0400

    review_contentBox UI done (animation not yet)

--- lifetrakr_cloncoding/main.js
+++ lifetrakr_cloncoding/main.js
@@ -2,8 +2,6 @@ let nav_moreBtn = document.querySelector("nav .moreBtn");
 let nav_menu = document.querySelector("nav .menu");
 let moreBtn_IsOn = false;
 
-console.log(nav_moreBtn);
-
 nav_moreBtn.addEventListener("click", () => {
     if(!moreBtn_IsOn){
         nav_menu.classList.add("is_on");
@@ -19,7 +17,7 @@ nav_moreBtn.addEventListener("click", () => {
 let items = document.querySelectorAll(".brief_contentBox .container .item");
 let selectedIndex = 0;
 
-console.log(items);
+
 
 let selectItem = (index) => {
     items[selectedIndex].classList.remove("is_on");
@@ -30,3 +28,40 @@ let selectItem = (index) => {
 for(let i = 0; i < items.length; i++){
     items[i].addEventListener("click", () => {  selectItem(i); }   );
 }
+
+
+/*=== review_contentBox ===*/
+let contents = document.querySelectorAll(".review_contentBox .content");
+let buttons = document.querySelectorAll(".review_contentBox button"); //0이 left 1이 right
+let currentIndex = 0;
+let prevIndex = 0;
+
+let openComment = () => {
+    contents[currentIndex].style.display = "flex";
+    contents[prevIndex].style.display = "none";
+}
+
+//leftBtn
+buttons[0].addEventListener("click", () => {
+    prevIndex = currentIndex;
+    currentIndex -= 1;
+    if(currentIndex < 0){
+        currentIndex = contents.length - 1;
+    }
+
+    openComment();
+});
+
+//rightBtn
+buttons[1].addEventListener("click", () => {
+    prevIndex = currentIndex;
+    currentIndex += 1;
+    if(currentIndex > contents.length - 1){
+        currentIndex = 0;
+    }
+
+    openComment();
+});
+
+
+
